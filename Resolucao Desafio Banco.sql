2afb45f29e5b9e0ab10182dcd955e491db935700
Heitor Porto <121198911+portoheitor@users.noreply.github.com>
2024-10-12 22:28:02 -0300

    Update Resolucao Desafio Banco.sql

--- Resolucao Desafio Banco.sql
+++ Resolucao Desafio Banco.sql
@@ -1,33 +1,33 @@
 --Filmes
----Tabela responsável por armazenar informações dos filmes.
+---Tabela responsÃ¡vel por armazenar informaÃ§Ãµes dos filmes.
 --Nome / Ano / Duracao
-SELECT * FROM DBO.Filmes
+--SELECT * FROM DBO.Filmes
 
 
 --Atores
----Tabela responsável por armazenar informações dos atores.
+---Tabela responsÃ¡vel por armazenar informaÃ§Ãµes dos atores.
 -- PrimeiroNome / UltimoNome / Genero
-SELECT * FROM DBO.Atores
+--SELECT * FROM DBO.Atores
 
 
 --Generos
---Tabela responsável por armazenar os gêneros dos filmes.
+--Tabela responsÃ¡vel por armazenar os gÃªneros dos filmes.
 -- Genero
-SELECT * FROM DBO.Generos
+--SELECT * FROM DBO.Generos
 
 
 --ElencoFilme
------Tabela responsável por representar um relacionamento do tipo muitos para muitos entre filmes e atores,
+-----Tabela responsÃ¡vel por representar um relacionamento do tipo muitos para muitos entre filmes e atores,
 --	 ou seja, um ator pode trabalhar em muitos filmes, e filmes podem ter muitos atores.
-SELECT * FROM DBO.ElencoFilme
+--SELECT * FROM DBO.ElencoFilme
 
 
 --FilmesGenero
------Tabela responsável por representar um relacionamento do tipo muitos para muitos entre filmes e gêneros,
---   ou seja, um filme pode ter mais de um gênero, e um genêro pode fazer parte de muitos filmes.
-SELECT * FROM DBO.FilmesGenero
+-----Tabela responsÃ¡vel por representar um relacionamento do tipo muitos para muitos entre filmes e gÃªneros,
+--   ou seja, um filme pode ter mais de um gÃªnero, e um genÃªro pode fazer parte de muitos filmes.
+--SELECT * FROM DBO.FilmesGenero
 
--- Desafio ->
+-- DESAFIO COMEÃ‡A AQUI ->
 
 -- 1-> Buscar o nome e ano dos filmes
 SELECT 
@@ -43,7 +43,7 @@ SELECT
 FROM Filmes
 ORDER BY Ano
 
--- 3-> Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+-- 3-> Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duraÃ§Ã£o
 SELECT
 	Nome,
 	Ano,
@@ -51,7 +51,7 @@ SELECT
 FROM Filmes
 WHERE Nome = 'De Volta para o Futuro'
 
--- 4-> Buscar os filmes lançados em 1997
+-- 4-> Buscar os filmes lanÃ§ados em 1997
 SELECT 
 	Nome,
 	Ano,
@@ -59,7 +59,7 @@ SELECT
 FROM Filmes
 WHERE Ano = 1997
 
--- 5-> Buscar os filmes lançados APÓS o ano 2000
+-- 5-> Buscar os filmes lanÃ§ados APÃ“S o ano 2000
 SELECT
 	Nome,
 	Ano,
@@ -76,7 +76,7 @@ FROM dbo.Filmes
 WHERE Duracao > 100 AND Duracao < 150
 ORDER BY Duracao
 
---7 -> Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
+--7 -> Buscar a quantidade de filmes lanÃ§adas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
 
 --SELECT * FROM Filmes ORDER BY Duracao DESC
 
@@ -88,13 +88,13 @@ GROUP BY Ano
 ORDER BY Quantidade DESC
 
 
--- 8-> Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+-- 8-> Buscar os Atores do gÃªnero masculino, retornando o PrimeiroNome, UltimoNome
 SELECT 
 	*
 FROM dbo.Atores
 WHERE Genero = 'M'
 
--- 9-> Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+-- 9-> Buscar os Atores do gÃªnero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
 
 SELECT 
 	*
@@ -102,7 +102,7 @@ FROM dbo.Atores
 WHERE Genero = 'F'
 ORDER BY PrimeiroNome
 
--- 10-> Buscar o nome do filme e o gênero
+-- 10-> Buscar o nome do filme e o gÃªnero
 SELECT
     Filmes.Nome,
     Generos.Genero 
@@ -111,14 +111,14 @@ FROM Filmes
     INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
 
 
--- 11-> Buscar o nome do filme e o gênero do tipo "Mistério"
+-- 11-> Buscar o nome do filme e o gÃªnero do tipo "MistÃ©rio"
 SELECT
 	Filmes.Nome,
 	Generos.Genero
 FROM Filmes
 	INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
 	INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
-WHERE Genero = 'Mistério'
+WHERE Genero = 'MistÃ©rio'
 
 
 -- 12-> Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
